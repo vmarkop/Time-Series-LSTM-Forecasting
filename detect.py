4615c8744213d6717514e5dc36cbd1f607dee72f
vmarkop <billy.mark.b.m.10@gmail.com>
2022-01-12 00:11:59 +0200

    functional detect.py (showing scaled data)

--- detect.py
+++ detect.py
@@ -5,6 +5,7 @@ import keras
 import random
 import pandas as pd
 import numpy as np
+import seaborn as sns
 # from keras.models import Sequential
 # from keras.layers import Dense
 # from keras.layers import LSTM
@@ -42,29 +43,41 @@ for i in range(0,n):
             selected_timeseries.append(ts)
             break
 
-# df = pd.read_csv('spx.csv', parse_dates=['date'], index_col='date')
+stock = dataframe.iloc[ts][0]
 timeseries = dataframe.iloc[ts,1:]
-df = pd.DataFrame(timeseries)
+df1 = pd.DataFrame(timeseries)
+df  = pd.DataFrame()
+
+dates = range(1,3650)
+dates = pd.DataFrame(dates)
+df['close'] = df1
+df['index'] = dates
+df.set_index('index')
 
 train_size = int(len(df) * 0.95)
 test_size = len(df) - train_size
 train, test = df.iloc[0:train_size], df.iloc[train_size:len(df)]
 print(train.shape, test.shape)
 
-
 scaler = StandardScaler()
-scaler = scaler.fit(train)
-train = scaler.transform(train)
-test = scaler.transform(test)
+scaler = scaler.fit(train[['close']])
 
+print(test[['close']].shape)
+
+test_close_backup = test[['close']]
+
+train['close'] = scaler.transform(train[['close']])
+test['close'] = scaler.transform(test[['close']])
+
+print(test[['close']].shape)
 
 def create_dataset(X, y, time_steps=1):
     Xs, ys = [], [] 
 
     for i in range(len(X) - time_steps):
-        v = X[i:(i + time_steps)] #X.iloc[i:(i + time_steps)].values
+        v = X.iloc[i:(i + time_steps)].values
         Xs.append(v)
-        ys.append(y[i + time_steps])#(y.iloc[i + time_steps])
+        ys.append(y.iloc[i + time_steps])
 
     return np.array(Xs), np.array(ys)
 
@@ -76,14 +89,14 @@ TIME_STEPS = 30
 print(train.shape)
 
 X_train, y_train = create_dataset(
-    train,
-    train.reshape(-1,1),
+    train[['close']],
+    train.close,
     TIME_STEPS
 )
 
 X_test, y_test = create_dataset(
-    test,
-    test.reshape(-1,1),
+    test[['close']],
+    test.close,
     TIME_STEPS
 )
 
@@ -129,11 +142,34 @@ X_test_pred = model.predict(X_test)
 test_mae_loss = np.mean(np.abs(X_test_pred - X_test), axis=1)
 
 
-test_score_df = pd.DataFrame(index=range(TIME_STEPS+train_size,len(df)))
+test_score_df = pd.DataFrame(index=test[TIME_STEPS:].index)
 test_score_df['loss'] = test_mae_loss
 test_score_df['threshold'] = THRESHOLD
 test_score_df['anomaly'] = test_score_df.loss > test_score_df.threshold
-test_score_df['close'] = test[TIME_STEPS:]
+test_score_df['close'] = test[TIME_STEPS:].close
 
 anomalies = test_score_df[test_score_df.anomaly == True]
 
+print("anomalies: ", len(anomalies))
+
+plt.plot(
+    test[TIME_STEPS:].index,
+    # test_close_backup[TIME_STEPS:],
+    test[TIME_STEPS:].close,
+    # scaler.inverse_transform(test[TIME_STEPS:].close),
+    label='close price'
+)
+
+if (len(anomalies)):
+    sns.scatterplot(
+        anomalies.index,
+        anomalies.close,
+        color=sns.color_palette()[3],
+        s=52,
+        label='anomaly'
+    )
+
+plt.xticks(rotation=25)
+plt.title(stock + ' Stock Anomaly Detection')
+plt.legend()
+plt.show()
